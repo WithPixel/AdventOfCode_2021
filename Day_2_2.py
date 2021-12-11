9437a7f1755bee55dc4ca950eb452a91a9869ced
WithPixel <81702586+WithPixel@users.noreply.github.com>
2021-12-11 15:04:55 +0100

    Add files via upload

---
+++ Day_2_2.py
@@ -0,0 +1,17 @@
+import csv
+
+with open('input.txt', encoding='utf-8') as File:
+    reader = csv.reader(File, delimiter = ';')
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+    for line in reader:
+        value = list(line)
+        if len(str(line)) == 13:
+            horizontal_position += int(value[0][8])
+            depth += aim * int(value[0][8])
+        elif len(str(line)) == 8:
+            aim -= int(value[0][3])
+        if len(str(line)) == 10:
+            aim += int(value[0][5])
+    print(horizontal_position * depth)
